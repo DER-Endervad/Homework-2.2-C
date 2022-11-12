e8367bdaa297b7d87aa6864b15d373b5da65d65c
Даниил <derendervad@gmail.com>
2022-11-12 21:13:14 +0300

    initial commit

---
+++ Homework 2.2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    struct client {
+        std::string name = "Пусто";
+        int number = 0;
+        int balance = 0;
+    };
+    client first;
+    std::cout << "Введите номер счёта : ";
+    std::cin >> first.number;
+    std::cout << "Введите имя владельца : "; 
+    std::cin >> first.name;
+    std::cout << "Введите баланс : ";
+    std::cin >> first.balance;
+    std::cout << "Введите новый баланс : ";
+    std::cin >> first.balance;
+    std::cout << first.name << std::endl;
+    std::cout << "Ваш счёт : " << first.name << ", " << first.number << ", " << first.balance << std::endl;
+}
